aff20febddcd51b2375f73f9e7338ef5746ab2b7
Paul Mathews <pfcmathews@gmail.com>
2017-02-09 15:09:42 +1300

    hihi successfully loads a graph and computes some numbers

--- kokako/detectors/hihi.py
+++ kokako/detectors/hihi.py
@@ -2,6 +2,7 @@
 TFGraphDetector with some hardcoded parameters to point to a pre-trained
 Hihi detector."""
 import os
+import numpy as np
 
 from kokako.detectors.tfgraph import TFGraphUser
 from kokako.score import Detector
@@ -25,10 +26,28 @@ class HihiCNN(Detector, TFGraphUser):
         """
         if not detector_path:
             detector_path = os.path.join(
-                os.path.dirname(__file__), 'model', 'hihi.pb')
+                os.path.dirname(__file__), 'models', 'hihi.pb')
 
-        super(HihiDetector, self).__init__(detector_path)
+        super(HihiCNN, self).__init__(detector_path)
+
+        # some constants
+        self._audio_chunk_size = 7680  # how many samples we deal with at once
+        self._audio_framerate = 24000  # expected sample rate of the audio
 
     def score(self, audio):
         """score some audio using the tensorflow graph"""
-        pass
+        # prepare the audio (convert to floating point, ensure the framerate)
+        audio_data = audio.audio  # assume 16 bit (the loading code does)
+        audio_data = audio_data.astype(np.float32) / (2**15)
+
+        if audio.framerate != self._audio_framerate:
+            print('framerate is wrong (expected {}, found {})'.format(
+                self._audio_framerate, audio.framerate))
+
+        result = self.average_graph_outputs(audio_data,
+                                            self._audio_chunk_size,
+                                            self._audio_hop_size)
+        # the hihi detector gives us mean and variance from the ensemble, so
+        # what should we do with them?
+        # for now just return the mean*100 to get it into a goodd range
+        return result[0] * 100
--- kokako/detectors/tfgraph.py
+++ kokako/detectors/tfgraph.py
@@ -3,6 +3,21 @@ import numpy as np
 import tensorflow as tf
 
 
+# TODO: this is hacky and fragile
+def _np_rfft(data):
+    """Does rfft with numpy"""
+
+    def _np_spectrum(data):
+        return np.fft.rfft(data).astype(np.complex64)
+
+    spectrum = tf.py_func(_np_spectrum, [data], [tf.complex64], stateful=False)
+    # figure out what the output shape should be so we can specify it directly
+    # (otherwise tf can't deal with py_func)
+    out_shape = data.get_shape().as_list()
+    out_shape[-1] = out_shape[-1] // 2 + 1
+    return tf.reshape(spectrum[0], out_shape)
+
+
 class TFGraphUser(object):
     """Base class for objects which load and run a tensorflow graph.
     The standard implementation does not run any initialisers, we assume the
@@ -30,7 +45,10 @@ class TFGraphUser(object):
             hop_size = chunk_size
         num_samples = audio.shape[0]
         for i in range(0, num_samples, hop_size):
-            yield audio[i:i+chunk_size, ...]
+            chunk = audio[i:i+chunk_size, ...]
+            if chunk.shape[0] == chunk_size:
+                yield audio[i:i+chunk_size, ...]
+            # if not, we should just fall off the end
 
     def __init__(self, graphdef_path, input_name=None, output_name=None):
         """Initialise the basic graph handling. Tries to be as
@@ -61,9 +79,62 @@ class TFGraphUser(object):
         with self._graph.as_default():
             return_elements = [self._input_node_name, self._output_node_name]
             self._input_node, self._output_node = tf.import_graph_def(
-                graph_def, return_elements=return_elements)
+                self._graphdef, return_elements=return_elements)
 
-        self.session = tf.Session(graph=self._graph)
+        self._session = tf.Session(graph=self._graph)
+
+        # TODO: hacky and fragile
+        # at this stage we need to use numpy for ffts on the cpu, so we have to
+        # wrap it up. For this to work with a saved graph, we have to register
+        # the wrapper with tensorflows python function registry, which does not
+        # happen unless we make an op
+        _ = _np_rfft(tf.random_normal([123]))
+
+    # this is slightly sub-optimal, but we have to do our best to clean up
+    def __del__(self):
+        if self._session:
+            self._session.close()
+
+    @property
+    def output_shape(self):
+        return self._output_node.get_shape()
+
+    @property
+    def input_shape(self):
+        return self._input_node.get_shape()
+
+    def _run_graph(self, input_value):
+        """Run the graph once on a given input.
+
+        Args:
+            input_value (ndarray): the data to feed in.
+
+        Returns:
+            ndarray: the result returned by running the output node.
+        """
+        result = self._session.run(self._output_node,
+                                   {self._input_node: input_value})
+        return result
+
+    def collect_graph_outputs(self, audio, chunk_size, hop_size=None):
+        """Collects the outputs of the graph over chunks of audio. Expects the
+        audio to be in whateber format it is that the graph expects.
+
+        Args:
+            audio (ndarray): numpy array, we assume the first dimension is
+                time, but that is the only assumption.
+            chunk_size (int): the size of the chunks we break `audio` into.
+            hop_size (Optional[int]): the number of samples we skip before
+                breaking off a new chunk. This can be used to extract
+                overlapping patches. If not specified, is set to `chunk_size`,
+                resulting in non-overlapping paches.
+
+        Returns:
+            list: the result of the graph at each location.
+        """
+        results = [self._run_graph(chunk)
+                   for chunk in self.chunk_audio(audio, chunk_size, hop_size)]
+        return results
 
     def average_graph_outputs(self, audio, chunk_size, hop_size=None):
         """Averages the outputs of the graph over chunks of audio. Expects the
@@ -81,4 +152,6 @@ class TFGraphUser(object):
         Returns:
             scalar: the average
         """
-        pass
+        chunk_outputs = self.collect_graph_outputs(audio, chunk_size, hop_size)
+        chunk_outputs = np.array(chunk_outputs)
+        return np.mean(chunk_outputs, axis=0)
